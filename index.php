29fe63f892d5b252a3280f84ded64bf2b2f2791a
Cody <cody.e.wofford@gmail.com>
2013-09-10 20:35:14 -0700

    Hello World

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: ERIKA
- * Date: 9/10/13
- * Time: 8:13 PM
- * To change this template use File | Settings | File Templates.
- */
+//Hello World
+print 'hello world';
